38d1eb1770a089c1d02642106fe23a65f98eda2e
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2022-05-12 01:59:50 +0200

    POST imporoved

--- client.py
+++ client.py
@@ -85,14 +85,34 @@
 
 
 import socket
+from os.path import exists as file_exists
+
 
 HEADER = 64
-PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = socket.gethostbyname(socket.gethostname())
+
+print("Enter Request: ")
+request_input = input()
+entries = request_input.split(' ')
+method = entries[0]
+file_name = entries[1]
+host_name = entries[2]
+if host_name == "localhost":
+    SERVER = socket.gethostbyname(socket.gethostname())
+else:
+    SERVER = host_name
+if len(entries) == 4:
+    PORT = entries[3]
+else:
+    PORT = 80
+
+
+
+# PORT = 5050
+# SERVER = socket.gethostbyname(socket.gethostname())
 # SERVER = "127.0.0.1"
-ADDR = (SERVER, PORT)
+ADDR = (SERVER, int(PORT))
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
@@ -103,15 +123,44 @@ def send(msg):
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
+    # client.send(send_length)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+    body = client.recv(8192).decode(FORMAT)
+    _, contents = body.rsplit("\r\n", 1)
+    get_file = open(file_name[1:].replace('/', '.'), "w")
+    get_file.write(contents)
+    # for line in body:
+    #     get_file.write(line)
+    get_file.write("\r\n")
+    get_file.close()
+    print(body)
+    # print(client.recv(2048).decode(FORMAT))
 
 
-send('POST /test.txt HTTP/1.1\r\nHost: localhost\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\nAccept-Encoding: gzip, deflate, sdch\r\nAccept-Language: en-US,en;q=0.8')
-input()
-# send("Hello Everyone!")
-# input()
-# send("Hello Tim!")
+# send('GET /test.txt HTTP/1.1\r\nHost: localhost\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\nAccept-Encoding: gzip, deflate, sdch\r\nAccept-Language: en-US,en;q=0.8')
+if host_name == "localhost":
+    request = method + " " + file_name + " " + "HTTP/1.0\r\n" + "HOST: " + host_name + ":" + PORT + "\r\n\r\n"
+    # if method == "GET":
+    if method == "POST":
+        # request = method + " " + file_name + " " + "HTTP/1.0\r\n" + "HOST: " + host_name + ":" + PORT + "\r\n\r\n"
+        if file_exists(file_name):
+            t = open(file_name, "r")
+            for v in t.readlines():
+                request += v
+            t.close()
+    request += "\r\n"
 
-send(DISCONNECT_MESSAGE)
+else:
+    if method == "GET":
+        request = method + " " + file_name + " " + "HTTP/1.0\r\n" + "HOST: " + host_name + "\r\n\r\n"
+    elif method == "POST":
+        request = method + " / " + "HTTP/1.0\r\n" + "HOST: " + host_name + "\r\n\r\n"
+
+print(request)
+send(request)
+
+
+
+
+# send(DISCONNECT_MESSAGE)
+print("[CONNECTION CLOSED]")
--- server.py
+++ server.py
@@ -93,7 +93,7 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 # SERVER = "192.168.56.1"
 ADDR = (SERVER, PORT)
-HEADER = 64 # Message default length
+HEADER = 64  # Message default length
 FORMAT = 'utf-8' # Decode format
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
@@ -105,71 +105,86 @@ def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected")
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length: # First empty message
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                break
-                # connected = False
-            method_string, headers = parse_request(msg)
-            if headers["Connection"] == "close":  # Non-persistent
-                connected = False
-            response = select_method(method_string)
-            print(f"[{addr}]")
-            print(msg)
-            conn.send(response.encode(FORMAT))
+        # msg_length = conn.recv(HEADER).decode(FORMAT)
+        # if msg_length:  # First empty message
+        #     msg_length = int(msg_length)
+        msg = conn.recv(2048).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            break
+            # connected = False
+        method_string, headers, body = parse_request(msg)
+        # if headers["Connection"] is not None and headers["Connection"] == "close":  # Non-persistent
+        #     connected = False
+        response = select_method(method_string, body)
+        print(f"[{addr}]")
+        print(msg, "\n")
+        print(response)
+        conn.send(response.encode(FORMAT))
     conn.close()
     print(f"[CONNECTION CLOSED]")
 
 
-def select_method(method_string):
+def select_method(method_string, body):
     method, url, http_version = method_string.split(' ', 2)
     if method == "POST":
-        return post_request(url)
+        return post_request(url, http_version, body)
     elif method == "GET":
-        return get_request(url)
+        return get_request(url, http_version)
     else:
         pass
 
 
-def post_request(url):
+def post_request(url, http_version, body):
+    # _, file = url.split("/", 1)
+    # file_name, extension = file.rsplit(".", 1)
+    # response = ""
+    # if file_exists(file):
+    #     response += http_version + " 200 OK\r\n"
+    #     read_file = open(file, "r")
+    #     post_file_name = file_name + "_POST." + extension
+    #     post_file = open(post_file_name, "w")
+    #     for line in read_file.readlines():
+    #         response += line
+    #         post_file.write(line)
+    #     read_file.close()
+    #     post_file.close()
+    # else:
+    #     response += http_version + " 404 Not Found\r\n"
+    body = body.split("\r\n")
     _, file = url.split("/", 1)
     file_name, extension = file.rsplit(".", 1)
-    response = ""
-    if file_exists(file):
-        response += "HTTP/1.0 200 OK\r\n"
-        read_file = open(file, "r")
-        post_file_name = file_name + "_POST." + extension
-        post_file = open(post_file_name, "w")
-        for line in read_file.readlines():
-            response += line
-            post_file.write(line)
-        read_file.close()
-        post_file.close()
-    else:
-        response += "HTTP/1.0 404 Not Found\r\n"
+    file_name += "_posted"
+    file = file_name + extension
+    post_file = open(file, "w")
+    for line in body:
+        post_file.write(line)
+    post_file.write("\r\n")
+    post_file.close()
+
+    response = http_version + " 200 OK\r\n\r\n"
     return response
 
 
-def get_request(url):
+def get_request(url, http_version):
     _, file = url.split("/", 1)
     response = ""
     # os.environ['NO_PROXY'] = '127.0.0.1'
     # r = requests.get('http://localhost:5050/Server/test.txt')
     if file_exists(file):
-        response += "HTTP/1.0 200 OK\r\n"
+        response += http_version + " 200 OK\r\n\r\n"
         t = open(file, "r")
         for v in t.readlines():
             response += v
+        t.close()
     else:
-        response += "HTTP/1.0 404 Not Found\r\n"
+        response += http_version + " 404 Not Found\r\n\r\n"
     return response
 
 
 def parse_request(request):
     # pop the first line so we only process headers
-    method, headers = request.split('\r\n', 1)
+    header, body = request.split('\r\n\r\n', 1)
+    method_string, headers = header.split('\r\n', 1)
 
     # construct a message from the request string
     message = email.message_from_file(StringIO(headers))
@@ -179,7 +194,7 @@ def parse_request(request):
 
     # pretty-print the dictionary of headers
     # pprint.pprint(headers, width=160)
-    return method, headers
+    return method_string, headers, body
 
 
 def start():
@@ -187,7 +202,7 @@ def start():
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
